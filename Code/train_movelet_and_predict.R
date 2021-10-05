fc13483895c6d08c3bfe0c6418027e9017b0583d
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2021-10-05 07:47:50 -0400

    Add 1_ses_train_walk mode

--- Code/train_movelet_and_predict.R
+++ Code/train_movelet_and_predict.R
@@ -33,6 +33,16 @@ if(mode == "1_ses_train"){
   
   test.dat <- subset(pca.dat, ActivityName %in% c("Turn_Key","Open_Door","Eat_From_Bag","4._Med_Taking","5._Med_Taking","7._Med_Taking","8._Med_Taking",
                                                   "10._Med_Taking","11._Med_Taking", "13._Med_Taking", "14._Med_Taking", "15._Med_Taking"))
+}else if(mode == "2_ses_train"){
+  train.label <- c("6._Med_Taking",
+                   "9._Med_Taking",
+                   #"12._Med_Taking",
+                   "Walk",
+                   "Wash_Hands",
+                   "Look_around_in_bag_or_purse")
+  
+  test.dat <- subset(pca.dat, ActivityName %in% c("Turn_Key","Open_Door","Eat_From_Bag","4._Med_Taking","5._Med_Taking","7._Med_Taking","8._Med_Taking",
+                                                  "10._Med_Taking","11._Med_Taking", "13._Med_Taking", "14._Med_Taking", "15._Med_Taking"))
 }else if(mode == "5_ses_train"){
   train.label <- c("3._Patient_Choice",
                    "6._Med_Taking",
