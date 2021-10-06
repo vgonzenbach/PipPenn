0e3a3cc286a1e6d7ac299645ef62320edb3a8034
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2021-10-06 10:18:35 -0400

    Cross validate training mode: 1 med session

--- Code/train_movelet_and_predict.R
+++ Code/train_movelet_and_predict.R
@@ -12,77 +12,31 @@ dat <- readRDS(data.path)
 ## clean up data and perform pca
 pca.dat <- run_pca(dat)
 
-mode = args[2]
-
-if(mode == "1_ses_train"){
-  train.label <- c("6._Med_Taking",
-                   #"9._Med_Taking",
-                   #"12._Med_Taking",
-                   "Wash_Hands",
-                   "Look_around_in_bag_or_purse")
-  
-  test.dat <- subset(pca.dat, ActivityName %in% c("Turn_Key","Open_Door","Eat_From_Bag","4._Med_Taking","5._Med_Taking","7._Med_Taking","8._Med_Taking",
-                                                  "10._Med_Taking","11._Med_Taking", "13._Med_Taking", "14._Med_Taking", "15._Med_Taking"))
-}else if(mode == "1_ses_train_walk"){
-  train.label <- c("6._Med_Taking",
-                   #"9._Med_Taking",
-                   #"12._Med_Taking",
-                   "Walk",
-                   "Wash_Hands",
-                   "Look_around_in_bag_or_purse")
-  
-  test.dat <- subset(pca.dat, ActivityName %in% c("Turn_Key","Open_Door","Eat_From_Bag","4._Med_Taking","5._Med_Taking","7._Med_Taking","8._Med_Taking",
-                                                  "10._Med_Taking","11._Med_Taking", "13._Med_Taking", "14._Med_Taking", "15._Med_Taking"))
-}else if(mode == "2_ses_train"){
-  train.label <- c("6._Med_Taking",
-                   "9._Med_Taking",
-                   #"12._Med_Taking",
-                   "Walk",
-                   "Wash_Hands",
-                   "Look_around_in_bag_or_purse")
-  
-  test.dat <- subset(pca.dat, ActivityName %in% c("Turn_Key","Open_Door","Eat_From_Bag","4._Med_Taking","5._Med_Taking","7._Med_Taking","8._Med_Taking",
-                                                  "10._Med_Taking","11._Med_Taking", "13._Med_Taking", "14._Med_Taking", "15._Med_Taking"))
-}else if(mode == "5_ses_train"){
-  train.label <- c("3._Patient_Choice",
-                   "6._Med_Taking",
-                   "9._Med_Taking",
-                   "12._Med_Taking",
-                   "15._Med_Taking",
-                   "Walk",
-                   "Wash_Hands",
-                   "Look_around_in_bag_or_purse")
-  
-  test.dat <- subset(pca.dat, ActivityName %in% c("Turn_Key","Open_Door","Eat_From_Bag","4._Med_Taking","5._Med_Taking","7._Med_Taking","8._Med_Taking",
-                                                  "10._Med_Taking","11._Med_Taking", "13._Med_Taking", "14._Med_Taking"))
-  
-} else if(mode == "4_ses_train"){
-  train.label <- c("3._Patient_Choice",
-                   "6._Med_Taking",
-                   "9._Med_Taking",
-                   "12._Med_Taking",
-                   "Walk",
-                   "Wash_Hands",
-                   "Look_around_in_bag_or_purse")
-  
-  test.dat <- subset(pca.dat, ActivityName %in% c("Turn_Key","Open_Door","Eat_From_Bag","4._Med_Taking","5._Med_Taking","7._Med_Taking","8._Med_Taking",
-                                                  "10._Med_Taking","11._Med_Taking", "13._Med_Taking", "14._Med_Taking", "15._Med_Taking"))
-} else if(mode == "3_ses_train"){
-  train.label <- c("6._Med_Taking",
-                   "9._Med_Taking",
-                   "12._Med_Taking",
-                   "Walk",
-                   "Wash_Hands",
-                   "Look_around_in_bag_or_purse")
-  
-  test.dat <- subset(pca.dat, ActivityName %in% c("Turn_Key","Open_Door","Eat_From_Bag","4._Med_Taking","5._Med_Taking","7._Med_Taking","8._Med_Taking",
-                                                  "10._Med_Taking","11._Med_Taking", "13._Med_Taking", "14._Med_Taking", "15._Med_Taking"))
+splitWalk = function(df){
+  # Rename Activity 
+  ends = range(which(pca.dat$ActivityName == "Walk"))
+  mid = round(diff(ends)/2)
+  df$ActivityName[ends[1]:mid] = "Walk_train"
+  df$ActivityName[(mid + 1):ends[2]] = "Walk_test"
+  return(df)
 }
 
-## create movelet-ready data 
+pca.dat = splitWalk(pca.dat)
+
+med.ses = args[2]
+# turn digit into label
+label.ses = function(med.ses){
+  labels = unique(pca.dat$ActivityName)
+  ses.label = labels[grep(paste0("^", med.ses), labels)]
+  return(ses.label)
+}
+
+## train chapters
+train.label =  c(label.ses(med.ses), "Walk_train", "Wash_Hands","Look_around_in_bag_or_purse")
+
 movelet.dat <- Acceleration_Create2(pca.dat,subjectName= dat$PatientID[1],
                                     n_axes = 6,frequency=50) # 50 obs per second
-## train chapters
+
 for(i in 1:length(train.label)){
   from <- min(which(movelet.dat$Label == train.label[i])) 
   to <- max(which(movelet.dat$Label == train.label[i])) 
@@ -90,6 +44,11 @@ for(i in 1:length(train.label)){
                                                                                DATA = movelet.dat, n_axes = 6,
                                                                                ChapName = train.label[i]))
 }
+
+## create movelet-ready data 
+test.dat <- subset(pca.dat, ActivityName %in% c("Walk_test", "Turn_Key","Open_Door","Eat_From_Bag",
+                                                "4._Med_Taking","7._Med_Taking", "10._Med_Taking", "13._Med_Taking"))
+
 test.movelet.dat <- Acceleration_Create2(test.dat,subjectName=dat$PatientID[1],n_axes = 6,frequency=50) 
 
 #expand the prediction activity labels to include all trained chapters
@@ -97,8 +56,8 @@ pred <- Movelet_Pred2(test.movelet.dat, Act.Names=train.label, vote=TRUE)
 pred$Pred2 <- recode(pred$Pred, `1`=train.label[1], `2`=train.label[2],`3`=train.label[3],`4`=train.label[4], `5`=train.label[5])
 
 ## Evaluate prediction
-pred$test <- eval_prediction(test.movelet.dat, pred, n_control_acts = 3)
+pred$test <- eval_prediction(test.movelet.dat, pred, n_control_acts = 4)
 
 # Save it
-saveRDS(pred, file = sprintf("Results/%s_pred_%s.rds", tools::file_path_sans_ext(basename(data.path)), mode))
+saveRDS(pred, file = sprintf("Results/%s_pred_ses-%s.rds", tools::file_path_sans_ext(basename(data.path)), med.ses))
 
--- bsub_train_and_predict.sh
+++ bsub_train_and_predict.sh
@@ -1,9 +1,7 @@
-for f in $(find Data -name "*.rds")
-do
-	#bsub Rscript Code/train_movelet_and_predict.R $f "1_ses_train_walk"
-	bsub Rscript Code/train_movelet_and_predict.R $f "2_ses_train"
-	#bsub Rscript Code/train_movelet_and_predict.R $f "3_ses_train"
-	#bsub Rscript Code/train_movelet_and_predict.R $f "5_ses_train"
-	#bsub Rscript Code/train_movelet_and_predict.R $f "4_ses_train"
-	#bsub Rscript Code/train_movelet_and_predict.R $f "1_ses_train"
+for patient in $(find Data -name "patient_*.rds")
+do 
+    for ses in 5 #6 8 9 11 12
+    do
+        bsub -o logs -e logs Rscript Code/train_movelet_and_predict.R $patient $ses
+    done
 done
