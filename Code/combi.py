8816b557c79096cc81c48a41d42e1843d0fb005f
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2021-10-13 10:30:03 -0400

    Visualize results from n training sessions

--- Code/combi.py
+++ Code/combi.py
@@ -7,7 +7,12 @@ parser.add_argument('Size', metavar='r', type=str, help='the size of each combin
 args = parser.parse_args()
 r = args.Size
 
-combs = [str(i) for i in combinations([5,6,8,9,11,12], int(r))]
-lines = [line.replace('(', '').replace(')', '').replace(' ', '') for line in combs]
-for line in lines:
-    print(line)
+med_sessions = [5,6,8,9,11,12]
+if int(r) == 1:
+    for ses in med_sessions:
+        print(ses)
+elif int(r) > 0:
+    combs = [str(i) for i in combinations(med_sessions, int(r))]
+    lines = [line.replace('(', '').replace(')', '').replace(' ', '') for line in combs]
+    for line in lines:
+        print(line)
